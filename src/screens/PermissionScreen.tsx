440e23ee3fd65dc9860a793647cb37fcaef5e6f5
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2021-06-09 01:27:27 -0500

    se agrego la solicitud de permisos al usuario

--- src/screens/PermissionScreen.tsx
+++ src/screens/PermissionScreen.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {Button, Platform, StyleSheet, Text, View} from 'react-native';
-import {check, PERMISSIONS, PermissionStatus} from 'react-native-permissions';
+import {PERMISSIONS, PermissionStatus, request} from 'react-native-permissions';
 
 export const PermissionScreen = () => {
   const chekLocationPermission = async () => {
     let permissionStatus: PermissionStatus;
     if (Platform.OS === 'ios') {
-      permissionStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      permissionStatus = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
     } else {
-      permissionStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      permissionStatus = await request(
+        PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+      );
     }
 
-    console.log(permissionStatus);
+    console.log({permissionStatus});
   };
   return (
     <View style={styles.container}>
